76e8c3c64017aa9c5acc4c46405fecb9c0f01519
RafayMemon01 <rafayrashid200611@gmail.com>
2024-09-10 11:28:42 +0500

    update post

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -1,22 +1,21 @@
-import express from 'express';
-import { Blog } from '../models/blog.js';
+import express from "express";
+import { Blog } from "../models/blog.js";
 
 const router = express.Router();
 
 // Route to get all blog posts
-router.get('/feed', async (req, res) => {
+router.get("/feed", async (req, res) => {
   try {
     const blogs = await Blog.find();
     res.json(blogs);
-    console.log("feeds sended")
+    console.log("feeds sended");
   } catch (error) {
     res.status(500).json({ message: "Error fetching blog posts", error });
   }
 });
 
-router.post('/add-post', async (req, res) => {
+router.post("/add-post", async (req, res) => {
   try {
-    
     const { title, content, author } = req.body;
     const newBlog = new Blog({ title, content, author });
     await newBlog.save();
@@ -24,9 +23,9 @@ router.post('/add-post', async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: "Error fetching blog posts", error });
   }
-})
+});
 
-router.delete('/delete-post/:id', async (req, res) => {
+router.delete("/delete-post/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const deletedBlog = await Blog.findByIdAndDelete(id);
@@ -36,6 +35,23 @@ router.delete('/delete-post/:id', async (req, res) => {
     res.json({ message: "Blog post deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: "Error deleting blog post", error });
+  }
+});
+
+router.put("/update-post/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, content, author } = req.body;
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      id,
+      { title, content, author },
+    );
+    if (!updatedBlog) {
+      return res.status(404).json({ message: "Blog post not found" });
+    }
+    res.json(updatedBlog);
+  } catch (error) {
+    res.status(500).json({ message: "Error updating blog post", error });
   }
 });
 
